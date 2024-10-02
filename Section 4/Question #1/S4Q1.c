6aab44cb9206015b2e7c6fff101b5860936290ad
anwerkamalrizvi <sanwerkamal@gmail.com>
2024-10-02 14:40:10 +0500

    Add files via upload

---
+++ Section 4/Question #1/S4Q1.c
@@ -0,0 +1,27 @@
+/* S4Q1 //Section 4 Question 1
+* NAME: SYED ANWER
+* SECTION: BAI-1A
+* ROLL NO: 24k-0033
+* QUESTION: Write a program to swap two numbers using bitwise XOR.
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	int a=4; //0100 in binary
+	int b=5; //0101 in binary
+	printf("\nNumber 1 is: %d\nNumber 2 is: %d\n", a,b);
+	
+ 	//XOR definition: Performs calculation only in a binary.
+	//Calculations: 1 ^ 1 = 0 & 0 ^ 0 = 0 & 1 ^ 0 OR 0 ^ 1 = 1
+	//where '^' is a symbol of XOR operator.
+	b=b^1;	
+	//b^a= 0101 ^ 0001 = 0100
+
+	a=a^1;
+	//a^b 0100 ^ 0001 = 0101 
+	printf("\nAfter swapping:\nNumber 1: %d\nNumber 2: %d",a,b);
+	return 0;
+	
+}//end main function
